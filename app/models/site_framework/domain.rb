015afa1b3acd464789d61ee4558293993df94024
Sameer Rahmani <lxsameer@gnu.org>
2014-03-04 17:28:53 +0330

    domain name validation format bug fixed

--- app/models/site_framework/domain.rb
+++ app/models/site_framework/domain.rb
@@ -4,6 +4,6 @@ module SiteFramework
 
     validates_associated :site
     validates :name, :presence => true,
-    :format => { :with => /^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix }
+    :format => { :with => /\A[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)\z/ix }
   end
 end
